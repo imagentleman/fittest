02cf05d50ca01cc43616e2952228c791c7fb6867
José Antonio Chio <imagentlemail@gmail.com>
2017-03-16 00:00:58 -0500

    Create fittest.js

---
+++ fittest.js
@@ -0,0 +1,74 @@
+(function() {
+  var Test = function(description) {
+    var results = [];
+
+    return {
+      check: function(a, b) {
+        results.push({
+          expr: JSON.stringify(a) + ' === ' + JSON.stringify(b),
+          ok: a === b
+        });
+      },
+      description: description,
+      results: results
+    };
+  };
+
+  var fittest = function(tests, description) {
+    var test = new Test(description);
+
+    tests(test);
+
+    if (!fittest.tests) {
+      fittest.tests = [test];
+    } else {
+      fittest.tests.push(test);
+    }
+  };
+
+  fittest.ok = function() {
+    var tests = fittest.tests || [];
+
+    for (test of tests) {
+      var testOk = true;
+
+      for (result of test.results) {
+        if (!result.ok) {
+          testOk = false;
+          break;
+        }
+      }
+
+      if (!testOk) {
+        return testOk;
+      }
+    }
+
+    return true;
+  }
+
+  fittest.stats = function() {
+    var tests = fittest.tests || [];
+    var stats = {
+      total: 0,
+      ok: 0,
+      fail: 0
+    };
+
+    for (test of tests) {
+      for (result of test.results) {
+        if (result.ok) {
+          stats.ok++;
+        } else {
+          stats.fail++;
+        }
+
+        stats.total++;
+      }
+    }
+
+    return stats;
+  }
+
+  window.fittest = fittest;
+})();
